b8e777bba9c73988238afa63b95278ecedadf43c
Davlik <slon.zelenyj@gmail.com>
2023-02-15 17:46:14 +0600

    feat: create function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrNumbers = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arrNumbers.push(expr.slice(i, i + 10));
+    }
+    let arrDotsAndDashs = [];
+    for (let j = 0; j < arrNumbers.length; j++) {
+        if (arrNumbers[j] === '**********') {
+            arrDotsAndDashs.push(' ');
+        }
+        let arrTwoDig = [];
+        for (let k = 0; k < 10; k += 2) {
+            if (`${arrNumbers[j][k]}${arrNumbers[j][k + 1]}` === '11') {
+                arrTwoDig.push('-');
+            }
+            if (`${arrNumbers[j][k]}${arrNumbers[j][k + 1]}` === '10') {
+                arrTwoDig.push('.');
+            }
+            if (`${arrNumbers[j][k]}${arrNumbers[j][k + 1]}` === '00') {
+                arrTwoDig.push('');
+            }
+        }
+        arrDotsAndDashs.push(arrTwoDig.join(''));
+    }
+    let string = [];
+    arrDotsAndDashs.forEach((symb) => {
+        if (symb === ' ' || symb === '') {
+            string.push(symb);
+        } else {
+            string.push(MORSE_TABLE[symb]);
+        }
+    })
+    return string.join('');
 }
 
 module.exports = {
